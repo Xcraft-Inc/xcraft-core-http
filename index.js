459f5edd7ba3b09651c1ec868c162dbd51f1b0b9
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-07-03 08:32:57 +0200

    Move zogLog, zogHttp, zogFs and zogPlatform in node_modules.

---
+++ index.js
@@ -0,0 +1,38 @@
+
+var moduleName = 'http';
+
+var fs   = require ('fs');
+var path = require ('path');
+
+var zogLog = require ('../lib/zogLog.js') (moduleName);
+
+exports.get = function (fileUrl, outputFile)
+{
+  var http  = require ('http');
+  var url   = require ('url');
+  var zogFs = require ('./zogFs.js');
+
+  var options =
+  {
+    host: url.parse (fileUrl).host,
+    port: 80,
+    path: url.parse (fileUrl).pathname
+  };
+
+  zogFs.mkdir (path.dirname (outputFile));
+
+  var file = fs.createWriteStream (outputFile);
+
+  http.get (options, function (res)
+  {
+    res.on ('data', function (data)
+    {
+      file.write (data);
+    });
+
+    res.on ('end', function ()
+    {
+      file.end ();
+    });
+  });
+}
